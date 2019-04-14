8238fc0f3bdb8b43ff1f960f4af090d4e2f14af7
main.jsp <37949276+shuman123@users.noreply.github.com>
2019-04-14 17:08:48 +0800

    Update SringMvcHandler.java

--- springmvcwebproject/src/com/cn/sm/handler/SringMvcHandler.java
+++ springmvcwebproject/src/com/cn/sm/handler/SringMvcHandler.java
@@ -3,12 +3,12 @@ package com.cn.sm.handler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//afsd
+//afsd13
 @Controller
 public class SringMvcHandler {
 	@RequestMapping("welcome")
     public String welcome(){
-    	System.out.println("welc°¢·²´ïo333asfdadfme.....");
+    	System.out.println("welcÂ°Â¢Â·Â²Â´Ã¯o333asfdadfme.....");
     	return "success";
     	
     }
