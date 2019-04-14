140ef14a6389ae737de8d2a979ac6de07880138e
sm <15826542163@163.com>
2019-04-14 16:08:58 +0800

    发布项目

---
+++ springmvcwebproject/src/com/cn/sm/handler/SringMvcHandler.java
@@ -0,0 +1,15 @@
+package com.cn.sm.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class SringMvcHandler {
+	@RequestMapping("welcome")
+    public String welcome(){
+    	System.out.println("welcome.....");
+    	return "success";
+    	
+    }
+}
